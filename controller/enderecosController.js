c597cb8458ad4cbcd1152b9217b3b9a612e8496c
Pedro Abreu <opedroabreu21@gmail.com>
2024-10-28 14:09:02 -0300

    tentativa 2

--- controller/enderecosController.js
+++ controller/enderecosController.js
@@ -2,13 +2,14 @@ const pool = require('../config/db')
 
 exports.criarEndereco = async (req, res) => {
     const {rua, cep, bairro, cidade, estado, pais} = req.body
-    const enderecoCompleto = `${rua}, ${bairro} ${cep} - ${cidade}, ${estado} , ${pais},`
+    const enderecoCompleto = `${rua}, ${bairro}, ${cidade} ${cep} - ${estado} , ${pais}`
     
     try {
         const result = await pool.query(
         `INSERT INTO enderecos (enderecoCompleto, rua, cep, bairro, cidade, estado, pais) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
         [enderecoCompleto, rua, cep, bairro, cidade, estado, pais]
     )
+    res.status(201).json(result.rows[0])
     } catch (error) {
         console.log(error)
         res.status(500).json({Message: "Impossivel criar endereco"})
@@ -18,6 +19,7 @@ exports.criarEndereco = async (req, res) => {
 exports.listarTodosOsEnderecos = async (req, res) => {
     try {
         const result = await pool.query(`SELECT * FROM ENDERECOS`)
+        res.status(201).json(result.rows[0])
     } catch (error) {
         console.log(error)
         res.status(500).json({Message: "Impossivel listar enderecos"})
@@ -25,10 +27,12 @@ exports.listarTodosOsEnderecos = async (req, res) => {
 }
 
 exports.buscarEndereco = async (req, res) => {
+    const {busca} = req.params
+
     try {
-        const result = await pool.query(`SELECT * FROM ENDERECOS `)
+        const result = await pool.query(`SELECT * FROM ENDERECOS WHERE enderecoCompleto = %${busca}%`)
     } catch (error) {
         console.log(error)
-        res.status(500).json({Message: "Impossivel listar enderecos"})
+        res.status(500).json({Message: "Impossivel listar endereco"})
     }
 }
