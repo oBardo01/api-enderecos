61b98f3d0e2f527e42aeb0bac1a5750fb6bcadbd
unknown <Aluno@L3T08.acad.etecpoa>
2024-11-07 22:18:22 -0300

    AIN'T NOTHING BUT A HEARTACHE

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const enderecosController = require('../controller/enderecosController.js')
 router.post('/criarEndereco', enderecosController.criarEndereco);
 router.get('/listar', enderecosController.listarTodosOsEnderecos);
 router.get('/buscar', enderecosController.buscarEndereco);
+router.get('/buscar/:id', enderecosController.buscarEnderecoPorId);
 router.get('/buscar/:filtro', enderecosController.buscarEnderecoFiltrado);
 router.put('/atualizar/:id', enderecosController.atualizarEndereco);
 router.delete('/deletar/:id', enderecosController.deletarEndereco);
