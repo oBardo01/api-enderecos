f3e1c303c24025419a4d2cc3818f3d4f624e8d06
unknown <Aluno@L3T08.acad.etecpoa>
2024-11-07 22:20:56 -0300

    TEEEL ME WHY

--- routes/routes.js
+++ routes/routes.js
@@ -5,8 +5,8 @@ const enderecosController = require('../controller/enderecosController.js')
 router.post('/criarEndereco', enderecosController.criarEndereco);
 router.get('/listar', enderecosController.listarTodosOsEnderecos);
 router.get('/buscar', enderecosController.buscarEndereco);
-router.get('/buscar/:id', enderecosController.buscarEnderecoPorId);
-router.get('/buscar/:filtro', enderecosController.buscarEnderecoFiltrado);
+router.get('/buscarPorId/:id', enderecosController.buscarEnderecoPorId);
+router.get('/buscarPorFiltro/:filtro', enderecosController.buscarEnderecoFiltrado);
 router.put('/atualizar/:id', enderecosController.atualizarEndereco);
 router.delete('/deletar/:id', enderecosController.deletarEndereco);
 
